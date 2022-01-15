777e89a4c25861ba3caa9ec940be37c74480a886
AlexTamVo <70011864+AlexTamVo@users.noreply.github.com>
2022-01-15 13:52:48 -0800

    Initialize Files

---
+++ app.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            message: 'Test',
+            Adi: 'https://media.discordapp.net/attachments/274745789690019841/932010360725180486/88954D67-6904-4D13-B76E-57594692FBFA.JPG?width=345&height=675'
+        }
+    },
+})
+
+app.mount('#app')
